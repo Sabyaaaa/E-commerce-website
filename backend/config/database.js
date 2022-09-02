86955b19c9037ede2788c5ce94441936e2175932
sabya <sabya7011@gmail.com>
2022-09-03 00:22:40 +0530

    Making a connection to the database

---
+++ backend/config/database.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const connectDatabase = () => {
+    mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true }).then(
+        (data) => {
+            console.log(`MongoDB connected with server: ${data.connection.host}`);
+        }).catch((err) => {
+            console.log(err)
+        })
+}
+
+module.exports = connectDatabase
