8a9c1af8caf600e6a285c58703d177dad01b364f
sabya <sabya@sabyas-MacBook-Air.local>
2022-09-02 17:34:57 +0530

    created routes and controller dir

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,8 @@
 const app = require("./app");
+const dotenv = require("dotenv");
+
+//config
+dotenv.config({ path: "backend/config/congig.env" })
 
 app.listen(process.env.PORT, () => {
     console.log('Server is working on http://localhost:${process.env.PORT}')
