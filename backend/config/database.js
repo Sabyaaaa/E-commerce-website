536c6a70a414a9a4cf6fd0de8ee41a767b920b8a
sabya <sabya7011@gmail.com>
2022-09-07 18:20:16 +0530

    Handling uncaught exception

--- backend/config/database.js
+++ backend/config/database.js
@@ -7,8 +7,6 @@ const connectDatabase = () => {
     }).then(
         (data) => {
             console.log(`MongoDB connected with server: ${data.connection.host}`);
-        }).catch((err) => {
-            console.log(err)
         })
 }
 
--- backend/server.js
+++ backend/server.js
@@ -2,16 +2,18 @@ const app = require("./app");
 const dotenv = require("dotenv");
 const connectDatabase = require("./config/database");
 
+//Handling Uncaught Exception
+process.on("uncaughtException", (err) => {
+
+    console.log(`Error: ${err.message}`);
+    console.log(`Shutting down he server due to Uncaught Exception`);
+});
 
 //config
 dotenv.config({ path: "backend/config/config.env" })
 
 //connecting to database
 connectDatabase();
-
-app.listen(process.env.PORT, () => {
-    console.log(`Server is working on http://localhost:${process.env.PORT}`)
-});
 
 //Unhandled Promise Exception (what if there is a error in the db_path link)
 process.on("unhandledRejection", (err) => {
@@ -22,4 +24,8 @@ process.on("unhandledRejection", (err) => {
     server.close(() => {
         process.exit(1);
     });
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is working on http://localhost:${process.env.PORT}`)
 });
