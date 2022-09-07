f32956c677645045fd48e63fb2e33b3bac58aca5
sabya <sabya7011@gmail.com>
2022-09-07 16:35:13 +0530

    Created delete product func and its route

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { getAllProducts, createProduct, updateProduct } = require("../controllers/productController");
+const { getAllProducts, createProduct, updateProduct, deleteProduct, getProductDetails } = require("../controllers/productController");
 
 
 const router = express.Router();
@@ -8,6 +8,6 @@ router.route("/products").get(getAllProducts)
 
 router.route("/product/new").post(createProduct);
 
-router.route("/product/:id").put(updateProduct);
+router.route("/product/:id").put(updateProduct).delete(deleteProduct).get(getProductDetails);
 
 module.exports = router;
