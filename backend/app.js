7c5fb471d51a0998918287770d60b75edd35710f
sabya <sabya7011@gmail.com>
2022-09-07 16:44:15 +0530

    Created error middleware

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const errorMiddleware = require("./middleware/error");
 
 app.use(express.json())
 
@@ -7,5 +8,8 @@ app.use(express.json())
 const product = require("./routes/productRoute");
 
 app.use("/api/v1", product);
+
+//Middleware for Errors
+app.use(errorMiddleware)
 
 module.exports = app
