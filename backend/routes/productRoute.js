1bff688d80574c61889fdc44111094740f1673d2
sabya <sabya7011@gmail.com>
2022-09-07 15:48:19 +0530

    Created update product method and its route

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { getAllProducts, createProduct } = require("../controllers/productController");
+const { getAllProducts, createProduct, updateProduct } = require("../controllers/productController");
 
 
 const router = express.Router();
@@ -7,5 +7,7 @@ const router = express.Router();
 router.route("/products").get(getAllProducts)
 
 router.route("/product/new").post(createProduct);
+
+router.route("/product/:id").put(updateProduct);
 
 module.exports = router;
