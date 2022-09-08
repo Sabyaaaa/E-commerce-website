c4a0dceccc6f303cd562e5cd6ac51c334b958c75
sabya <sabya7011@gmail.com>
2022-09-08 15:35:08 +0530

    creating register user func

---
+++ backend/controllers/userController.js
@@ -0,0 +1,24 @@
+const ErrorHandler = require("../utils/errorhandler");
+const catchAsyncErrors = require("../middleware/catchAsyncErrors");
+const User = require("../models/userModel");
+
+//Register a User
+exports.registerUser = catchAsyncErrors(async (req, res, next) => {
+
+    const { name, email, password } = req.body;
+
+    const user = await user.create({
+        name,
+        email,
+        password,
+        avtar: {
+            public_id: "This is a sample id",
+            url: "profilePicUrl"
+        }
+    });
+
+    res.status(201).json({
+        success: true,
+        user
+    });
+});
